8545baed0434e3ce25b9b0f5543dd2cbced47c08
Lucas Basquerotto <lucas@99freelas.com.br>
2024-02-15 16:04:31 -0300

    .

---
+++ gradio-demo/save_image_random_name.py
@@ -0,0 +1,15 @@
+import random
+import string
+import gradio as gr 
+
+def save_image_random_name(image):
+    random_string = ''.join(random.choices(string.ascii_letters, k=20)) + '.png'
+    image.save(random_string)
+    print(f"Saved image to {random_string}!")
+
+demo = gr.Interface(
+    fn=save_image_random_name, 
+    inputs=gr.Image(type="pil"), 
+    outputs=None,
+)
+demo.launch()
