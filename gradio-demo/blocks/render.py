dc4a52e54b75c42752e6782e84f2b343522dbf91
Lucas Basquerotto <lucas@99freelas.com.br>
2024-02-16 15:27:58 -0300

    .

---
+++ gradio-demo/blocks/render.py
@@ -0,0 +1,9 @@
+import gradio as gr
+
+input_textbox = gr.Textbox()
+
+with gr.Blocks() as demo:
+    gr.Examples(["hello", "bonjour", "merhaba"], input_textbox)
+    input_textbox.render()
+
+demo.launch()
